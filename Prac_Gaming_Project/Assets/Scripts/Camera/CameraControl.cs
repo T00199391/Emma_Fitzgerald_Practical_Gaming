acfa4ab250d83c6254f33778455a8fa931cb35cd
T00199391 <t00199391@students.ittralee.ie>
2019-02-16 22:02:41 +0000

    Made changes to the movement of the player character and gave the character an animation Signed-off-by: T00199391 <t00199391@students.ittralee.ie>

---
+++ Prac_Gaming_Project/Assets/Scripts/Camera/CameraControl.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public Transform target;
+    public Vector3 offset;
+    public bool useOffsetValues;
+    public float rotateSpeed;
+    public Transform pivot;
+
+	// Use this for initialization
+	void Start () {
+        if (!useOffsetValues)
+        {
+            offset = target.position - transform.position;
+        }
+
+        pivot.transform.position = target.transform.position;
+        //pivot.transform.parent = target.transform;
+        pivot.transform.parent = null;
+
+        Cursor.lockState = CursorLockMode.Locked;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        pivot.transform.position = target.transform.position;
+        //Get the x position of the mouse and rotate the target
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        pivot.Rotate(0, horizontal, 0);
+        //Get the x position of the mouse and rotate the pivot
+        //float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+        //pivot.Rotate(-vertical, 0, 0);
+
+        //Move the camera based on the current rotation  of the target and the original offset
+        float desiredYAngle = pivot .eulerAngles.y;
+        //float desiredXAngle = pivot.eulerAngles.x;
+        Quaternion rotation = Quaternion.Euler(0, desiredYAngle, 0);
+        transform.position = target.position - (rotation * offset);
+
+        //transform.position = target.position - offset;
+        transform.LookAt(target.transform);
+    }
+}
---
+++ Prac_Gaming_Project/Assets/Scripts/Player/TestMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestMovement : MonoBehaviour {
+
+    public float moveSpeed;
+    public CharacterController controller;
+    private Vector3 moveDirection;
+    public float gravityScale;
+    public Animator anim;
+    public Transform pivot;
+    public float rotateSpeed;
+    public GameObject playerModel;
+        
+	// Use this for initialization
+	void Start () {
+        controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveDirection = (transform.forward * Input.GetAxis("Vertical") * moveSpeed) + (transform.right * Input.GetAxis("Horizontal") * moveSpeed);
+        moveDirection = moveDirection.normalized * moveSpeed;
+
+        //moveDirection = new Vector3(Input.GetAxis("Horizontal") * moveSpeed, 0f, Input.GetAxis("Vertical") * moveSpeed);
+
+        controller.Move(moveDirection * Time.deltaTime);
+
+        //Move the player is different directions based on camera look direction
+        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, pivot.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
+
+        anim.SetFloat("Speed", (Mathf.Abs(Input.GetAxis("Vertical")) + Mathf.Abs(Input.GetAxis("Horizontal"))));
+	}
+}
