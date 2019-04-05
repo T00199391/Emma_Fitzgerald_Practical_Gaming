73a9973777bc9a660a818f00eab7f3a2f66ab22b
Emma P. Fitzgerald <Emma.P.Fitzgerald@students.ittralee.ie>
2019-04-05 11:00:35 +0100

    Made it that you can switch scenes when you click a button on the menu Signed-off-by: Emma P. Fitzgerald <Emma.P.Fitzgerald@students.ittralee.ie>

---
+++ Prac_Gaming_Project/Assets/Scenes/SceneSwitcher.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    //https://loekvandenouweland.com/content/use-unity-button-to-change-between-scenes.html
+    public void GotoGameScene()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void GotoControlScene()
+    {
+        SceneManager.LoadScene("Controls");
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
